d5626ae8e852bf1ad6f656ebe58357783b71b550
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-13 00:14:57 +0200

    feat: handle errors when connecting/disconnecting wallet

--- src/features/wallet/useWallet.ts
+++ src/features/wallet/useWallet.ts
@@ -1,13 +1,29 @@
 import { useCelo } from '@celo/react-celo';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
 export function useWallet() {
   const { address, connect, destroy } = useCelo();
   const isConnected: boolean = useMemo(() => !!address, [address]);
 
+  const connectWallet = useCallback(async () => {
+    try {
+      await connect();
+    } catch (error) {
+      console.error(error);
+    }
+  }, [connect]);
+
+  const disconnectWallet = useCallback(async () => {
+    try {
+      await destroy();
+    } catch (error) {
+      console.error(error);
+    }
+  }, [destroy]);
+
   return {
-    connectWallet: connect,
-    disconnectWallet: destroy,
+    connectWallet,
+    disconnectWallet,
     isConnected,
   };
 };
