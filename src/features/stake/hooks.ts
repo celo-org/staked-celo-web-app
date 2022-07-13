a0f4a008463d754cc02b949dcb49baf6952278fc
Krystian Fras <krystian@fras.me>
2022-07-13 16:45:33 +0200

    Remove unused code

--- src/features/stake/hooks.ts
+++ src/features/stake/hooks.ts
@@ -1,20 +1,3 @@
-import BigNumber from 'bignumber.js'
-
-export type ExchangeValueFormatter = (fromAmount: BigNumber | null | undefined) => string
-
-const valueFormatter = (...args: any[]) => {
-  // eslint-disable-next-line no-console
-  console.log(args)
-  return {
-    value: 12,
-    rate: 1.2,
-  }
-}
-
-export const useStake = () => ({
-  valueFormatter,
-})
-
 const estDepositValue = (amount: number) => amount * 1.03
 
 export const useEstimate = () => ({
