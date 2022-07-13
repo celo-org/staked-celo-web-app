0092ce5c3c87a76e2d1f2537595a244b6f5e6d2f
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-13 13:11:52 +0200

    feat: add simple Modal component

---
+++ src/components/modals/Modal.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import { PropsWithChildren } from 'react';
+import ReactModal from 'react-modal';
+import Close from 'src/images/icons/close.svg';
+
+interface ModalProps {
+  isOpen: boolean
+  close: () => void
+  screenReaderLabel?: string
+}
+
+export default function Modal({
+  isOpen,
+  screenReaderLabel,
+  close,
+  children,
+}: PropsWithChildren<ModalProps>) {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      overlayClassName="fixed bg-gray-100 bg-opacity-75 inset-0"
+      contentLabel={screenReaderLabel}
+      onRequestClose={close}
+      shouldCloseOnOverlayClick={true}
+    >
+      <div className="flex flex-col h-full">
+        <div className="flex justify-end">
+          <span style={{ cursor: 'pointer' }} onClick={close}>
+            <Image src={Close} alt="Close button" width={18} height={18} />
+          </span>
+        </div>
+        { children }
+      </div>
+    </ReactModal>
+  );
+}
