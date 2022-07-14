df04fe49bde4b1ff815ee197f5e2cb8ab92c90d6
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-14 13:31:48 +0200

    feat: add 'logger' service

---
+++ src/services/logger.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const logger = {
+  warn: (...args: any[]) => console.warn(...args),
+  error: (...args: any[]) => console.error(...args),
+};
+/* eslint-enable no-console */
+
+export default logger;
