d02364e628f7a6e96b632b484fbc32aa44a70aea
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-14 18:14:59 +0200

    feat: add method to estimate stCELO amount from CELO

--- src/features/stake/useEstimate.ts
+++ src/features/stake/useEstimate.ts
@@ -1,7 +1,22 @@
+import { useCelo } from '@celo/react-celo';
+import BigNumber from 'bignumber.js';
+import { useStake } from 'src/features/stake/useStake';
+
 const estDepositValue = (amount: number) => amount * 1.03;
 
 export function useEstimate() {
+  const { address } = useCelo();
+  const { managerContract } = useStake();
+
+  const estStCELO = async (celoAmount: BigNumber) => {
+    const stCELOAmount = await managerContract.methods
+      .toStakedCelo(celoAmount.toString())
+      .call({ from: address });
+    return stCELOAmount;
+  };
+
   return {
+    estStCELO,
     estDepositValue,
   };
 }
