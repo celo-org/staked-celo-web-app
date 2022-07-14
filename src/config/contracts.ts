7f462cccbc478dd7bc96905b63b5722d24c35ab6
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-14 16:31:28 +0200

    feat: add useStake hook with manager contract instance

---
+++ src/config/contracts.ts
@@ -0,0 +1,1 @@
+export const managerAddress: string = process.env.NEXT_PUBLIC_MANAGER_ADDRESS as string;
---
+++ src/features/stake/useStake.ts
@@ -0,0 +1,18 @@
+import { useCelo } from '@celo/react-celo';
+import { useMemo } from 'react';
+import ManagerABI from 'src/blockchain/ABIs/Manager.json';
+import { managerAddress } from 'src/config/contracts';
+import { AbiItem } from 'web3-utils';
+
+export function useStake() {
+  const { kit } = useCelo();
+
+  const managerContract = useMemo(() => {
+    const { eth } = kit.connection.web3;
+    return new eth.Contract(ManagerABI as AbiItem[], managerAddress);
+  }, [kit.connection]);
+
+  return {
+    managerContract,
+  };
+}
