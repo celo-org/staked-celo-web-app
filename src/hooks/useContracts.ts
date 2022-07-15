25a67a6b27c9ec5dfc6af24b4be1580aee1155d6
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-15 15:55:47 +0200

    feat: add Account contract to useContracts

--- src/hooks/useContracts.ts
+++ src/hooks/useContracts.ts
@@ -1,8 +1,9 @@
 import { useCelo } from '@celo/react-celo';
 import { useMemo } from 'react';
+import AccountABI from 'src/blockchain/ABIs/Account.json';
 import ManagerABI from 'src/blockchain/ABIs/Manager.json';
 import StakedCeloABI from 'src/blockchain/ABIs/StakedCelo.json';
-import { managerAddress, stakedCeloAddress } from 'src/config/contracts';
+import { accountAddress, managerAddress, stakedCeloAddress } from 'src/config/contracts';
 import { AbiItem } from 'web3-utils';
 
 export function useContracts() {
@@ -18,8 +19,14 @@ export function useContracts() {
     return new eth.Contract(StakedCeloABI as AbiItem[], stakedCeloAddress);
   }, [kit.connection]);
 
+  const accountContract = useMemo(() => {
+    const { eth } = kit.connection.web3;
+    return new eth.Contract(AccountABI as AbiItem[], accountAddress);
+  }, [kit.connection]);
+
   return {
     managerContract,
     stakedCeloContract,
+    accountContract,
   };
 }
