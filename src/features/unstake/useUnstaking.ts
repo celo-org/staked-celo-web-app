a9df0d214038cd305b3a52054268c9ef70f9e9a5
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-15 15:46:00 +0200

    feat: add useUnstaking with methods to unstake staked Celo

---
+++ src/features/unstake/useUnstaking.ts
@@ -0,0 +1,34 @@
+import { useCelo } from '@celo/react-celo';
+import BigNumber from 'bignumber.js';
+import { useContracts } from 'src/hooks/useContracts';
+
+export function useUnstaking() {
+  const { address } = useCelo();
+  const { managerContract } = useContracts();
+
+  const createTxOptions = () => ({
+    from: address,
+  });
+
+  const withdraw = (stakedCeloAmount: BigNumber) =>
+    managerContract.methods.withdraw(stakedCeloAmount.toString());
+
+  const unstake = (stakedCeloAmount: BigNumber) =>
+    withdraw(stakedCeloAmount).send(createTxOptions());
+
+  const estimateUnstakingFee = async (stakedCeloAmount: BigNumber): Promise<BigNumber> => {
+    const estimatedFee = new BigNumber(
+      await withdraw(stakedCeloAmount).estimateGas(createTxOptions())
+    );
+    return estimatedFee.plus(estimatedFee.dividedBy(10));
+  };
+
+  const estimateCeloWithdrawal = async (stakedCeloAmount: BigNumber) =>
+    managerContract.methods.toCelo(stakedCeloAmount.toString()).call({ from: address });
+
+  return {
+    unstake,
+    estimateUnstakingFee,
+    estimateCeloWithdrawal,
+  };
+}
