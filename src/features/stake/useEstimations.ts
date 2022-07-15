81fa3bbef0dc1159e5d474b86dfc91e4d28904b6
Paweł Jakubowski <p.m.jakubowski@gmail.com>
2022-07-15 15:45:04 +0200

    Add methods for staking (#12)
    * feat: add useStaking hook with stake method
    
    * refactor: change 'est' prefix of methods to 'estimate'
    
    * feat: add method to estimate fee of staking transaction
    
    * refactor: move estimateStCELO method to useStaking as estimateStakedCeloDeposit

--- src/features/stake/useEstimations.ts
+++ src/features/stake/useEstimations.ts
@@ -1,22 +1,7 @@
-import { useCelo } from '@celo/react-celo';
-import BigNumber from 'bignumber.js';
-import { useContracts } from 'src/hooks/useContracts';
-
-const estDepositValue = (amount: number) => amount * 1.03;
+const estimateDepositValue = (amount: number) => amount * 1.03;
 
 export function useEstimations() {
-  const { address } = useCelo();
-  const { managerContract } = useContracts();
-
-  const estStCELO = async (celoAmount: BigNumber) => {
-    const stCELOAmount = await managerContract.methods
-      .toStakedCelo(celoAmount.toString())
-      .call({ from: address });
-    return stCELOAmount;
-  };
-
   return {
-    estStCELO,
-    estDepositValue,
+    estimateDepositValue,
   };
 }
---
+++ src/features/stake/useStaking.ts
@@ -0,0 +1,31 @@
+import { useCelo } from '@celo/react-celo';
+import BigNumber from 'bignumber.js';
+import { useContracts } from 'src/hooks/useContracts';
+
+export function useStaking() {
+  const { address } = useCelo();
+  const { managerContract } = useContracts();
+
+  const createTxOptions = (celoAmount: BigNumber) => ({
+    from: address,
+    value: celoAmount.toString(),
+  });
+
+  const deposit = () => managerContract.methods.deposit();
+
+  const stake = (celoAmount: BigNumber) => deposit().send(createTxOptions(celoAmount));
+
+  const estimateStakingFee = async (celoAmount: BigNumber): Promise<BigNumber> => {
+    const estimatedFee = new BigNumber(await deposit().estimateGas(createTxOptions(celoAmount)));
+    return estimatedFee.plus(estimatedFee.dividedBy(10));
+  };
+
+  const estimateStakedCeloDeposit = async (celoAmount: BigNumber) =>
+    managerContract.methods.toStakedCelo(celoAmount.toString()).call({ from: address });
+
+  return {
+    stake,
+    estimateStakingFee,
+    estimateStakedCeloDeposit,
+  };
+}
