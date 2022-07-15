bb09f261de7511e93d2def49d67e42d11f07c6b8
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-15 15:53:04 +0200

    feat: add StakedCelo contract

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -1,1 +1,2 @@
 export const managerAddress: string = process.env.NEXT_PUBLIC_MANAGER_ADDRESS as string;
+export const stakedCeloAddress: string = process.env.NEXT_PUBLIC_STAKED_CELO_ADDRESS as string;
--- src/hooks/useContracts.ts
+++ src/hooks/useContracts.ts
@@ -1,7 +1,8 @@
 import { useCelo } from '@celo/react-celo';
 import { useMemo } from 'react';
 import ManagerABI from 'src/blockchain/ABIs/Manager.json';
-import { managerAddress } from 'src/config/contracts';
+import StakedCeloABI from 'src/blockchain/ABIs/StakedCelo.json';
+import { managerAddress, stakedCeloAddress } from 'src/config/contracts';
 import { AbiItem } from 'web3-utils';
 
 export function useContracts() {
@@ -12,7 +13,13 @@ export function useContracts() {
     return new eth.Contract(ManagerABI as AbiItem[], managerAddress);
   }, [kit.connection]);
 
+  const stakedCeloContract = useMemo(() => {
+    const { eth } = kit.connection.web3;
+    return new eth.Contract(StakedCeloABI as AbiItem[], stakedCeloAddress);
+  }, [kit.connection]);
+
   return {
     managerContract,
+    stakedCeloContract,
   };
 }
