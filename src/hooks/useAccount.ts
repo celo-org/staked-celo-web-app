de7a1821e41a60f535528c2fbf2bf34ac0ca0585
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-15 16:02:25 +0200

    feat: add support for staked Celo balance in useAccount

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,9 +1,11 @@
 import { useCelo } from '@celo/react-celo';
 import BigNumber from 'bignumber.js';
 import { useCallback, useState } from 'react';
+import { useContracts } from 'src/hooks/useContracts';
 
 export function useAccount() {
   const { address: _address, kit } = useCelo();
+  const { stakedCeloContract } = useContracts();
   const address = _address ?? undefined;
 
   const [celoBalance, setCeloBalance] = useState(new BigNumber(0));
@@ -17,9 +19,20 @@ export function useAccount() {
     setCeloBalance(new BigNumber(balance));
   }, [kit.connection, address]);
 
+  const [stakedCeloBalance, setStakedCeloBalance] = useState(new BigNumber(0));
+
+  const loadStakedCeloBalance = useCallback(async () => {
+    const stakedCeloBalance = await stakedCeloContract.methods.balanceOf(address).call({
+      from: address,
+    });
+    setStakedCeloBalance(new BigNumber(stakedCeloBalance));
+  }, [address, stakedCeloContract]);
+
   return {
     address,
     celoBalance,
     loadCeloBalance,
+    stakedCeloBalance,
+    loadStakedCeloBalance,
   };
 }
