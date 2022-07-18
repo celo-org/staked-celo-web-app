cda8c5fc46d897f268e46fee9e85a148ebd9a92f
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-18 15:22:20 +0200

    refactor: use useCallback for callbacks

--- src/features/stake/useStaking.ts
+++ src/features/stake/useStaking.ts
@@ -1,4 +1,5 @@
 import BigNumber from 'bignumber.js';
+import { useCallback } from 'react';
 import { toWei } from 'src/formatters/amount';
 import { useAccount } from 'src/hooks/useAccount';
 import { useContracts } from 'src/hooks/useContracts';
@@ -7,27 +8,36 @@ export function useStaking() {
   const { address } = useAccount();
   const { managerContract } = useContracts();
 
-  const createTxOptions = (celoAmount: BigNumber) => ({
-    from: address,
-    value: celoAmount.toString(),
-  });
+  const createTxOptions = useCallback(
+    (celoAmount: BigNumber) => ({
+      from: address,
+      value: celoAmount.toString(),
+    }),
+    [address]
+  );
 
-  const deposit = () => managerContract.methods.deposit();
+  const deposit = useCallback(() => managerContract.methods.deposit(), [managerContract]);
 
-  const stake = (celoAmount: BigNumber) => deposit().send(createTxOptions(celoAmount));
+  const stake = useCallback(
+    (celoAmount: BigNumber) => deposit().send(createTxOptions(celoAmount)),
+    [createTxOptions, deposit]
+  );
 
-  const estimateStakingFee = async (celoAmount: BigNumber): Promise<BigNumber> => {
-    const estimatedFee = new BigNumber(await deposit().estimateGas(createTxOptions(celoAmount)));
-    return estimatedFee.plus(estimatedFee.dividedBy(10));
-  };
+  const estimateStakingFee = useCallback(
+    async (celoAmount: BigNumber): Promise<BigNumber> => {
+      const estimatedFee = new BigNumber(await deposit().estimateGas(createTxOptions(celoAmount)));
+      return estimatedFee.plus(estimatedFee.dividedBy(10));
+    },
+    [createTxOptions, deposit]
+  );
 
-  const getStakedCeloExchageRate = async (): Promise<BigNumber> => {
+  const getStakedCeloExchageRate = useCallback(async (): Promise<BigNumber> => {
     const oneCeloInWei = toWei(new BigNumber('1'));
     const stakedCeloAmount = new BigNumber(
       await managerContract.methods.toStakedCelo(oneCeloInWei).call({ from: address })
     );
     return stakedCeloAmount.dividedBy(oneCeloInWei);
-  };
+  }, [managerContract, address]);
 
   return {
     stake,
