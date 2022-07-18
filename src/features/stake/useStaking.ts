401967211b4ac52d293751c96825ff84165d9bab
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-18 14:05:21 +0200

    feat: replace 'estimateStakedCeloDeposit' with 'getStakedCeloExchageRate'

--- src/features/stake/useStaking.ts
+++ src/features/stake/useStaking.ts
@@ -1,4 +1,5 @@
 import BigNumber from 'bignumber.js';
+import { toWei } from 'src/formatters/amount';
 import { useAccount } from 'src/hooks/useAccount';
 import { useContracts } from 'src/hooks/useContracts';
 
@@ -20,12 +21,17 @@ export function useStaking() {
     return estimatedFee.plus(estimatedFee.dividedBy(10));
   };
 
-  const estimateStakedCeloDeposit = (celoAmount: BigNumber) =>
-    managerContract.methods.toStakedCelo(celoAmount.toString()).call({ from: address });
+  const getStakedCeloExchageRate = async (): Promise<BigNumber> => {
+    const oneCeloInWei = toWei(new BigNumber('1'));
+    const stakedCeloAmount = new BigNumber(
+      await managerContract.methods.toStakedCelo(oneCeloInWei).call({ from: address })
+    );
+    return stakedCeloAmount.dividedBy(oneCeloInWei);
+  };
 
   return {
     stake,
     estimateStakingFee,
-    estimateStakedCeloDeposit,
+    getStakedCeloExchageRate,
   };
 }
