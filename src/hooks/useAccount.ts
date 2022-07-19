9a9b00fb0a8ccbfda2a09f50545808747700c6e4
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-19 09:10:32 +0200

    refactor: move all useAccount hook logic into AccountProvider

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,39 +1,15 @@
-import { useCelo } from '@celo/react-celo';
-import BigNumber from 'bignumber.js';
-import { useCallback, useContext } from 'react';
-import { useContracts } from 'src/hooks/useContracts';
+import { useContext } from 'react';
 import { AccountContext } from 'src/providers/AccountProvider';
 
 export function useAccount() {
-  const { address: _address, kit } = useCelo();
-  const { celoBalance, setCeloBalance, stakedCeloBalance, setStakedCeloBalance } =
+  const { isConnected, address, celoBalance, stakedCeloBalance, loadBalances } =
     useContext(AccountContext);
-  const { stakedCeloContract } = useContracts();
-  const address = _address ?? undefined;
-  const isConnected = !!address;
-
-  const loadCeloBalance = useCallback(async () => {
-    const { eth } = kit.connection.web3;
-    if (!address) return;
-
-    const balance = await eth.getBalance(address);
-
-    setCeloBalance(new BigNumber(balance));
-  }, [kit.connection, address, setCeloBalance]);
-
-  const loadStakedCeloBalance = useCallback(async () => {
-    const stakedCeloBalance = await stakedCeloContract.methods.balanceOf(address).call({
-      from: address,
-    });
-    setStakedCeloBalance(new BigNumber(stakedCeloBalance));
-  }, [address, stakedCeloContract, setStakedCeloBalance]);
 
   return {
     isConnected,
     address,
     celoBalance,
-    loadCeloBalance,
     stakedCeloBalance,
-    loadStakedCeloBalance,
+    loadBalances,
   };
 }
