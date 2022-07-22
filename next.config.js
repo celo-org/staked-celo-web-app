1e2e41f69de1fda57ecc56ee1415d2e4e6b84b3c
Paweł Jakubowski <p.m.jakubowski@gmail.com>
2022-07-22 13:48:34 +0200

    feat: generate and serve third party licenses (#28)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
@@ -6,6 +8,11 @@ const nextConfig = {
     if (!isServer) {
       config.resolve.fallback.net = false;
     }
+    config.plugins.push(
+      new webpack.BannerPlugin({
+        banner: 'For third party licenses check /THIRD_PARTY_LICENSES.txt',
+      })
+    );
     return config;
   },
 };
