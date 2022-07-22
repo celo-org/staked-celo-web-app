db54997cfd3721ec8e8154371eabdf2b022c1d05
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-22 15:34:21 +0200

    feat: withdraw scheduled withdrawals in the background

--- src/hooks/useWithdrawals.ts
+++ src/hooks/useWithdrawals.ts
@@ -1,7 +1,37 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useContracts } from 'src/hooks/useContracts';
+import api from 'src/services/api';
 import { PendingWithdrawal } from 'src/types/account';
 import { Celo } from 'src/types/units';
+
+export const useWithdrawalBot = (address: string | null) => {
+  const { managerContract, accountContract } = useContracts();
+
+  const finalizeWithdrawal = useCallback(async () => {
+    if (!address) return;
+    const [activeGroups, deprecatedGroups] = await Promise.all([
+      managerContract.methods.getGroups().call(),
+      managerContract.methods.getDeprecatedGroups().call(),
+    ]);
+    const groups = [...activeGroups, ...deprecatedGroups];
+    for (const group of groups) {
+      const scheduledWithdrawals = await accountContract.methods
+        .scheduledWithdrawalsForGroupAndBeneficiary(group, address)
+        .call();
+      if (scheduledWithdrawals === '0') continue;
+      await api.withdraw(address);
+      return;
+    }
+  }, [address, managerContract, accountContract]);
+
+  useEffect(() => {
+    void finalizeWithdrawal();
+    const intervalId = setInterval(finalizeWithdrawal, 60 * 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [finalizeWithdrawal]);
+};
 
 export const useWithdrawals = (address: string | null) => {
   const { accountContract } = useContracts();
