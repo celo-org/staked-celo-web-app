0bee8841399b40242a156f2b18ac3c06d0592861
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-22 10:55:34 +0200

    refactor: remove unused loadExchangeRates from useExchangeContext hook

--- src/providers/ExchangeProvider.tsx
+++ src/providers/ExchangeProvider.tsx
@@ -4,24 +4,21 @@ import { useExchangeRates } from 'src/hooks/useExchangeRates';
 interface ExchangeContext {
   celoExchangeRate: number;
   stCeloExchangeRate: number;
-  loadExchangeRates: () => Promise<void>;
 }
 
 export const ExchangeContext = createContext<ExchangeContext>({
   celoExchangeRate: 0,
   stCeloExchangeRate: 0,
-  loadExchangeRates: () => Promise.resolve(),
 });
 
 export const ExchangeProvider = ({ children }: PropsWithChildren) => {
-  const { celoExchangeRate, stCeloExchangeRate, loadExchangeRates } = useExchangeRates();
+  const { celoExchangeRate, stCeloExchangeRate } = useExchangeRates();
 
   return (
     <ExchangeContext.Provider
       value={{
         celoExchangeRate,
         stCeloExchangeRate,
-        loadExchangeRates,
       }}
     >
       {children}
