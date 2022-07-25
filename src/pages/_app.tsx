b42261b7c68ada05f0ee51e2006c342991fc9335
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-25 15:40:30 +0200

    feat: install and configure dayjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import type { AppProps } from 'next/app';
 import { toast, ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,6 +8,8 @@ import { AppLayout } from 'src/layout/AppLayout';
 import { CeloProvider } from 'src/providers/CeloProvider';
 import 'src/styles/fonts.css';
 import 'src/styles/globals.css';
+
+dayjs.extend(relativeTime);
 
 const App = ({ Component, pageProps, router }: AppProps) => {
   const pathName = router.pathname;
