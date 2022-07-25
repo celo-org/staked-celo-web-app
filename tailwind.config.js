61bf543ef9dbf4c656c8cdbde1c408c7fa9c0192
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-07-25 15:39:59 +0200

    fix: extend Tailwind colors instead of replacing all of them

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,39 +4,40 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      // default
-      black: colors.black,
-      transparent: colors.transparent,
-      blue: colors.blue,
-      red: colors.red,
-      yellow: colors.amber,
-      pear: '#C8D72B',
-      teal: colors.teal,
-      // custom
-      green: '#7ED8C2',
-      red: '#F17256',
-      orange: {
-        DEFAULT: '#E7B65D',
-        700: '#E7B65D',
-        800: '#DFA849',
-        900: '#EEAF43',
+    extend: {
+      colors: {
+        // default
+        black: colors.black,
+        transparent: colors.transparent,
+        blue: colors.blue,
+        red: colors.red,
+        yellow: colors.amber,
+        pear: '#C8D72B',
+        teal: colors.teal,
+        // custom
+        green: '#7ED8C2',
+        red: '#F17256',
+        orange: {
+          DEFAULT: '#E7B65D',
+          700: '#E7B65D',
+          800: '#DFA849',
+          900: '#EEAF43',
+        },
+        purple: {
+          DEFAULT: '#A73EF9',
+          700: '#9781EE',
+          800: '#9477F5',
+          900: '#8C62FF',
+        },
+        gray: {
+          100: '#E5EAEE',
+          400: '#7B9799',
+          800: '#324142',
+          900: '#212B2E',
+        },
+        white: '#FFFFFF',
       },
-      purple: {
-        DEFAULT: '#A73EF9',
-        700: '#9781EE',
-        800: '#9477F5',
-        900: '#8C62FF',
-      },
-      gray: {
-        100: '#E5EAEE',
-        400: '#7B9799',
-        800: '#324142',
-        900: '#212B2E',
-      },
-      white: '#FFFFFF',
     },
-    extend: {},
   },
   plugins: [],
 };
