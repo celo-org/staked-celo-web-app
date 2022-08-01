19cdcdda8cc783970a23e35ff3b83882d0603226
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-01 15:14:24 +0200

    fix: set application viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,8 @@ class MyDocument extends Document {
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#35d07f" />
           <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-config" content="/browserconfig.xml" />
           <meta name="theme-color" content="#ffffff" />
