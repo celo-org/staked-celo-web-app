ab5ee6681b519b99635e0b0c06a69f052267f57e
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-03 18:44:04 +0200

    fix: add default value for 'classes' property

--- src/components/text/Label.tsx
+++ src/components/text/Label.tsx
@@ -4,6 +4,6 @@ interface LabelProps {
   classes?: string;
 }
 
-export const Label = ({ children, classes }: PropsWithChildren<LabelProps>) => {
+export const Label = ({ children, classes = '' }: PropsWithChildren<LabelProps>) => {
   return <span className={`text-[14px] leading-[16px] font-medium ${classes}`}>{children}</span>;
 };
