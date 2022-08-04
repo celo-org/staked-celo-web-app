7c2cfffd5b8db14955fc4ad692cbd67c0e7d73b3
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-04 17:02:54 +0200

    refactor: remove redundant meta viewport tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,8 +19,6 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.ico" />
           <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
 
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-config" content="/browserconfig.xml" />
 
