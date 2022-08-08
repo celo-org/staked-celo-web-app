4a670c7ac2f78781a99d403673e3844eb0e80bc0
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-08 10:23:56 +0200

    refactor: extract OpacityTransition component

--- src/components/icons/TokenIcon.tsx
+++ src/components/icons/TokenIcon.tsx
@@ -1,11 +1,8 @@
 import Image from 'next/image';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { OpacityTransition } from 'src/components/transitions/OpacityTransition';
 import CeloIcon from 'src/images/icons/token-celo.svg';
 import stCeloIcon from 'src/images/icons/token-stcelo.svg';
-import scssTransitions from 'src/styles/transitions.module.scss';
 import { TokenType } from 'src/utils/tokens';
-
-const { transitionDuration } = scssTransitions;
 
 const tokenIcon: { [key in TokenType]: any } = {
   CELO: CeloIcon,
@@ -20,17 +17,15 @@ interface TokenIconProps {
 }
 
 export const TokenIcon = ({ token, quality = 100, width = 32, height = 32 }: TokenIconProps) => (
-  <TransitionGroup mode="out-in" className="inline-flex relative">
-    <CSSTransition key={token} timeout={parseInt(transitionDuration)} classNames="opacity">
-      <span className="inline-flex">
-        <Image
-          src={tokenIcon[token]}
-          alt={`${token} logo`}
-          quality={quality}
-          width={width}
-          height={height}
-        />
-      </span>
-    </CSSTransition>
-  </TransitionGroup>
+  <OpacityTransition id={token} classes="inline-flex">
+    <span className="inline-flex">
+      <Image
+        src={tokenIcon[token]}
+        alt={`${token} logo`}
+        quality={quality}
+        width={width}
+        height={height}
+      />
+    </span>
+  </OpacityTransition>
 );
