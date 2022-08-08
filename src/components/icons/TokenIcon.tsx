16d1c8eec92211ff2616bde02346c9e4c080f5f8
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-08 10:22:58 +0200

    feat: add TokenIcon transition

--- src/components/icons/TokenIcon.tsx
+++ src/components/icons/TokenIcon.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import CeloIcon from 'src/images/icons/token-celo.svg';
 import stCeloIcon from 'src/images/icons/token-stcelo.svg';
+import scssTransitions from 'src/styles/transitions.module.scss';
 import { TokenType } from 'src/utils/tokens';
+
+const { transitionDuration } = scssTransitions;
 
 const tokenIcon: { [key in TokenType]: any } = {
   CELO: CeloIcon,
@@ -16,11 +20,17 @@ interface TokenIconProps {
 }
 
 export const TokenIcon = ({ token, quality = 100, width = 32, height = 32 }: TokenIconProps) => (
-  <Image
-    src={tokenIcon[token]}
-    alt={`${token} logo`}
-    quality={quality}
-    width={width}
-    height={height}
-  />
+  <TransitionGroup mode="out-in" className="inline-flex relative">
+    <CSSTransition key={token} timeout={parseInt(transitionDuration)} classNames="opacity">
+      <span className="inline-flex">
+        <Image
+          src={tokenIcon[token]}
+          alt={`${token} logo`}
+          quality={quality}
+          width={width}
+          height={height}
+        />
+      </span>
+    </CSSTransition>
+  </TransitionGroup>
 );
