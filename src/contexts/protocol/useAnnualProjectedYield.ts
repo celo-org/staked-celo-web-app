5c051152cd14e379f84c920254f237ea4c946e7b
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-10 19:12:04 +0200

    refactor: improve readability of APY calculation and add comments

--- src/contexts/protocol/useAnnualProjectedYield.ts
+++ src/contexts/protocol/useAnnualProjectedYield.ts
@@ -8,21 +8,27 @@ export const useAnnualProjectedYield = () => {
 
   const loadAnnualProjectedYield = useCallback(async () => {
     if (!epochRewardsContract) return;
-    const [rewardsMultiplier, { 0: targetVotingYield }] = await Promise.all([
+    const [rewardsMultiplierFraction, { 0: targetVotingYieldFraction }] = await Promise.all([
       epochRewardsContract.methods.getRewardsMultiplier().call(),
       epochRewardsContract.methods.getTargetVotingYieldParameters().call(),
     ]);
 
-    const divider = new BigNumber(10).pow(24);
-    const apy = new BigNumber(targetVotingYield)
-      .div(divider)
-      .plus(1)
-      .pow(365)
-      .minus(1)
-      .times(rewardsMultiplier)
-      .div(divider)
-      .times(100);
-    setAnnualProjectedYield(apy.toFixed(2));
+    // EpochRewards contract is using Fixidity library which operates on decimal part of numbers
+    // Fixidity is always using 24 length decimal parts
+    const fixidityDecimalSize = new BigNumber(10).pow(24);
+    const targetVotingYield = new BigNumber(targetVotingYieldFraction).div(fixidityDecimalSize);
+    const rewardsMultiplier = new BigNumber(rewardsMultiplierFraction).div(fixidityDecimalSize);
+
+    // Target voting yield is for a single day only, so we have to calculate this for entire year
+    // Check formula definition here: https://en.wikipedia.org/wiki/Annual_percentage_yield#Equation
+    const unadjustedAPY = targetVotingYield.plus(1).pow(365).minus(1);
+
+    // According to the protocol it has to be adjusted by rewards multiplier
+    const adjustedApy = unadjustedAPY.times(rewardsMultiplier);
+
+    const percentageAPY = adjustedApy.times(100);
+
+    setAnnualProjectedYield(percentageAPY.toFixed(2));
   }, [epochRewardsContract]);
 
   useEffect(() => {
