6c67b9682f2131edb325f37e2224b2c05aacb9b3
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-10 18:36:53 +0200

    refactor: remove unnecessary function aliases

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,6 +1,6 @@
 import BigNumber from 'bignumber.js';
 import { DISPLAY_DECIMALS, WEI_PER_UNIT } from 'src/config/consts';
-import { fromWei as web3FromToken, toWei as web3ToToken } from 'web3-utils';
+import { fromWei, toWei } from 'web3-utils';
 
 export type TokenType = 'CELO' | 'stCELO';
 
@@ -10,7 +10,7 @@ export class Token extends BigNumber {
   }
 
   format(): string {
-    const value = web3FromToken(this.toFixed(0, BigNumber.ROUND_FLOOR));
+    const value = fromWei(this.toFixed(0, BigNumber.ROUND_FLOOR));
     return new BigNumber(value)
       .toFormat(DISPLAY_DECIMALS, BigNumber.ROUND_FLOOR)
       .replace(/0*$/, '')
@@ -30,10 +30,10 @@ export function toToken(value: string): Token {
   if (!value) return new Token('0');
   const components = value.split('.');
   if (components.length === 1) {
-    return new Token(web3ToToken(value));
+    return new Token(toWei(value));
   } else if (components.length === 2) {
     const trimmedFraction = components[1].substring(0, WEI_PER_UNIT.length - 1);
-    return new Token(web3ToToken(`${components[0]}.${trimmedFraction}`));
+    return new Token(toWei(`${components[0]}.${trimmedFraction}`));
   } else {
     throw new Error(`Cannot convert ${value} to wei`);
   }
