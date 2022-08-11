29664096a0dedb6f2dfd6192918b13dc9ad32417
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-11 18:51:37 +0200

    feat: change contract addresses when changing network

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -1,3 +1,17 @@
-export const managerAddress: string = process.env.NEXT_PUBLIC_MANAGER_ADDRESS as string;
-export const stCeloAddress: string = process.env.NEXT_PUBLIC_STAKED_CELO_ADDRESS as string;
-export const accountAddress: string = process.env.NEXT_PUBLIC_ACCOUNT_ADDRESS as string;
+interface ContractAddresses {
+  manager: string;
+  stakedCelo: string;
+  account: string;
+}
+
+export const mainnetAddresses: ContractAddresses = {
+  manager: process.env.NEXT_PUBLIC_MANAGER_MAINNET_ADDRESS as string,
+  stakedCelo: process.env.NEXT_PUBLIC_STAKED_CELO_MAINNET_ADDRESS as string,
+  account: process.env.NEXT_PUBLIC_ACCOUNT_MAINNET_ADDRESS as string,
+};
+
+export const testnetAddresses: ContractAddresses = {
+  manager: process.env.NEXT_PUBLIC_MANAGER_TESTNET_ADDRESS as string,
+  stakedCelo: process.env.NEXT_PUBLIC_STAKED_CELO_TESTNET_ADDRESS as string,
+  account: process.env.NEXT_PUBLIC_ACCOUNT_TESTNET_ADDRESS as string,
+};
