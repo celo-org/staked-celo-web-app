889ea6d3836614116d38b0255fac538257e04c48
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-12 13:50:29 +0200

    feat: change page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps, router }: AppProps) => {
   return (
     <>
       <Head>
-        <title>{`Liquid Staking | ${getHeadTitle(pathName)}`}</title>
+        <title>{`StakedCelo - Liquid staking on Celo | ${getHeadTitle(pathName)}`}</title>
       </Head>
       <ClientOnly>
         <TopProvider>
