16ca61217ea8ded92466cbbfe7ef49a07f4498ac
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-12 13:49:48 +0200

    refactor: move page title setting to App component

--- src/layout/AppLayout/index.tsx
+++ src/layout/AppLayout/index.tsx
@@ -4,18 +4,6 @@ import { useThemeContext } from 'src/contexts/theme/ThemeContext';
 import { Footer } from './Footer';
 import { Header } from './Header';
 
-function toTitleCase(str: string) {
-  return str.replace(/\w\S*/g, (txt) => {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-}
-
-function getHeadTitle(pathName: string) {
-  const segments = pathName.split('/');
-  if (segments.length <= 1 || !segments[1]) return 'Home';
-  else return toTitleCase(segments[1]);
-}
-
 function isConnectPage(pathName: string) {
   return pathName === '/connect';
 }
@@ -33,7 +21,6 @@ export const AppLayout = ({ pathName, children }: PropsWithChildren<Props>) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* colors are the same as --c-bg-primary-color in styles/globals */}
         <meta name="theme-color" content={theme === 'dark' ? '#212B2E' : '#FFFDF4'} />
-        <title>{`Liquid Staking | ${getHeadTitle(pathName)}`}</title>
       </Head>
       <div className="flex flex-col h-full min-w-screen min-w-[320px] sm:min-h-screen text-color-primary overflow-x-hidden">
         <div className="flex flex-col min-h-screen sm:min-h-full sm:flex-grow">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@celo/react-celo/lib/styles.css';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { PropsWithChildren, useEffect, useState } from 'react';
 import { toast, ToastContainer, Zoom } from 'react-toastify';
@@ -20,18 +21,33 @@ const App = ({ Component, pageProps, router }: AppProps) => {
   const pathName = router.pathname;
 
   return (
-    <ClientOnly>
-      <TopProvider>
-        <CeloConnectRedirect>
-          <AppLayout pathName={pathName}>
-            <Component {...pageProps} />
-            <ToastContainer transition={Zoom} position={toast.POSITION.TOP_CENTER} icon={false} />
-          </AppLayout>
-        </CeloConnectRedirect>
-      </TopProvider>
-    </ClientOnly>
+    <>
+      <Head>
+        <title>{`Liquid Staking | ${getHeadTitle(pathName)}`}</title>
+      </Head>
+      <ClientOnly>
+        <TopProvider>
+          <CeloConnectRedirect>
+            <AppLayout pathName={pathName}>
+              <Component {...pageProps} />
+              <ToastContainer transition={Zoom} position={toast.POSITION.TOP_CENTER} icon={false} />
+            </AppLayout>
+          </CeloConnectRedirect>
+        </TopProvider>
+      </ClientOnly>
+    </>
   );
 };
+
+function getHeadTitle(pathName: string) {
+  const segments = pathName.split('/');
+  if (segments.length <= 1 || !segments[1]) return 'Home';
+
+  return segments[1].replace(
+    /\w\S*/g,
+    (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  );
+}
 
 // https://github.com/vercel/next.js/issues/2473#issuecomment-587551234
 const ClientOnly = ({ children }: PropsWithChildren) => {
