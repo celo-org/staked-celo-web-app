6dbfab60efd75c3c805938aa34ca689bf1eb4e98
Paweł Jakubowski <pawel.jakubowski@tooploox.com>
2022-08-17 14:16:22 +0200

    feat: update Footer according to the latest design

--- src/config/externalUrls.ts
+++ src/config/externalUrls.ts
@@ -2,3 +2,4 @@ export const twitterUrl: string = process.env.NEXT_PUBLIC_TWITTER_URL as string;
 export const discordUrl: string = process.env.NEXT_PUBLIC_DISCORD_URL as string;
 export const githubUrl: string = process.env.NEXT_PUBLIC_GITHUB_URL as string;
 export const docsUrl: string = process.env.NEXT_PUBLIC_DOCS_URL as string;
+export const privacyUrl: string = process.env.NEXT_PUBLIC_PRIVACY_URL as string;
--- src/layout/AppLayout/Footer.tsx
+++ src/layout/AppLayout/Footer.tsx
@@ -1,60 +1,78 @@
 import Link from 'next/link';
 import { ThemedIcon } from 'src/components/icons/ThemedIcon';
 import { Label } from 'src/components/text/Label';
-import { discordUrl, docsUrl, githubUrl, twitterUrl } from 'src/config/externalUrls';
+import { discordUrl, docsUrl, githubUrl, privacyUrl, twitterUrl } from 'src/config/externalUrls';
 import { useProtocolContext } from 'src/contexts/protocol/ProtocolContext';
-
-const FAQLinks = () => {
-  const textClasses = 'text-[18px] leading-[32px] font-medium underline';
-  return (
-    <>
-      <div className={`${textClasses} sm:mb-0 mb-[16px] sm:mr-[24px]`}>
-        <Link href="/faq">FAQ</Link>
-      </div>
-      <div className={textClasses}>
-        <a href={docsUrl} target="_blank" rel="noreferrer">
-          Docs
-        </a>
-      </div>
-    </>
-  );
-};
 
 export const Footer = () => {
   const { totalCeloBalance } = useProtocolContext();
 
   const footerFlexClasses = 'flex flex-col sm:flex-row sm:items-center sm:justify-between';
   return (
-    <footer className={`w-full ${footerFlexClasses} p-base sm:h-[168px] bg-secondary`}>
-      <div className="flex flex-col sm:order-first mb-[32px] sm:mb-0 sm:w-1/3">
-        <Label classes="mb-[4px]">Total CELO staked</Label>
-        <span className="text-[27px] leading-[40px]">{totalCeloBalance.displayAsBase()}</span>
+    <footer className={`w-full ${footerFlexClasses} p-base sm:h-[180px] bg-secondary`}>
+      <div className="flex flex-col sm:h-[100%] justify-between items-start mb-[16px] sm:mb-0">
+        <div className="flex flex-col sm:mb-0 mb-[32px]">
+          <Label classes="mb-[4px]">Total CELO staked</Label>
+          <span className="text-[27px] leading-[40px]">{totalCeloBalance.displayAsBase()}</span>
+        </div>
+        <FAQLinks classes="mb-[64px] sm:hidden" />
+        <div className="inline-flex">
+          <SocialMediaLink type="twitter" url={twitterUrl} classes="mr-[16px]" />
+          <SocialMediaLink type="discord" url={discordUrl} classes="mr-[16px]" />
+          <SocialMediaLink type="github" url={githubUrl} />
+        </div>
       </div>
-
-      <div className="mb-[64px] sm:order-last sm:hidden">{FAQLinks()}</div>
-
-      <ul className="flex mb-[16px] sm:mb-0 sm:order-2">
-        <li className="mr-[16px]">
-          <a href={twitterUrl} target="_blank" rel="noreferrer">
-            <ThemedIcon name="twitter" alt="Twitter logo" quality={100} width={40} height={40} />
-          </a>
-        </li>
-        <li className="mr-[16px]">
-          <a href={discordUrl} target="_blank" rel="noreferrer">
-            <ThemedIcon name="discord" alt="Discord logo" quality={100} width={40} height={40} />
-          </a>
-        </li>
-        <li>
-          <a href={githubUrl} target="_blank" rel="noreferrer">
-            <ThemedIcon name="github" alt="Github logo" quality={100} width={40} height={40} />
-          </a>
-        </li>
-      </ul>
-
-      <span className="text-[16px] leading-[24px] font-normal sm:order-last sm:w-1/3 sm:text-right">
-        <div className="hidden sm:flex flex-row justify-end mb-[24px]">{FAQLinks()}</div>
+      <div className="sm:h-[100%] text-[16px] leading-[24px] font-normal sm:text-right">
+        <FAQLinks classes="hidden sm:flex flex-row justify-end mb-[24px]" />
         Open source Apache 2 <br /> © 2022 cLabs, Inc.
-      </span>
+      </div>
     </footer>
+  );
+};
+
+interface FAQLinksProps {
+  classes: string;
+}
+
+const FAQLinks = ({ classes }: FAQLinksProps) => {
+  const linkPositionClasses = 'sm:mb-0 mb-[16px] sm:mr-[24px]';
+  return (
+    <div className={`text-[18px] leading-[32px] font-medium underline ${classes}`}>
+      <div className={linkPositionClasses}>
+        <Link href="/faq">FAQ</Link>
+      </div>
+      <div className={linkPositionClasses}>
+        <a href={docsUrl} target="_blank" rel="noreferrer">
+          Docs
+        </a>
+      </div>
+      <div className={linkPositionClasses}>
+        <a href={privacyUrl} target="_blank" rel="noreferrer">
+          Privacy
+        </a>
+      </div>
+      <div>
+        <Link href="/terms">Terms</Link>
+      </div>
+    </div>
+  );
+};
+
+interface SocialMediaLinkProps {
+  type: 'github' | 'twitter' | 'discord';
+  url: string;
+  classes?: string;
+}
+
+const SocialMediaLink = ({ type, url, classes = '' }: SocialMediaLinkProps) => {
+  return (
+    <a
+      className={`inline-flex w-[40px] h-[40px] sm:w-[32px] sm:h-[32px] ${classes}`}
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <ThemedIcon name={type} alt={type} quality={100} fill />
+    </a>
   );
 };
