c88647cf837cae72a8359273d40674ab3b48391f
Aaron <aaron.deruvo@clabs.co>
2023-03-29 18:57:09 -0500

    Add Pill Component

---
+++ src/components/pills/Pill.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from 'react';
+
+interface LabelProps {
+  classes?: string;
+}
+
+export const Pill = ({ children, classes = '' }: PropsWithChildren<LabelProps>) => {
+  return (
+    <span
+      className={`rounded-lg px-2 py-1 bg-action-secondary-regular text-[12px] leading-[14px] font-medium ${classes}`}
+    >
+      {children}
+    </span>
+  );
+};
