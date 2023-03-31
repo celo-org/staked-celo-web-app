9d481024a1aed9a65db8ef7a23508a28c2cc4078
Aaron <aaron.deruvo@clabs.co>
2023-03-31 13:06:16 -0500

    add validatorGroupRow component

---
+++ src/features/validators/ValidatorGroupRow.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Row } from 'src/components/list/row';
+const ADDRESS_SLICE_POINT_A = 5;
+const ADDRESS_SLICE_POINT_B = 38;
+const ADDRESS_SLICE_POINT_LAST_PART = 37;
+
+interface Props {
+  name: string;
+  groupAddress: string;
+  isCurrentStrategy: boolean
+}
+
+export const ValidatorGroupRow = React.memo(({ name, groupAddress, isCurrentStrategy }: Props) => {
+  const displayName = name.length === 0 ? removeAddressMiddle(groupAddress) : name;
+  const href = `validators/${groupAddress}`;
+
+  const truncatedAddress = groupAddress.slice(ADDRESS_SLICE_POINT_LAST_PART);
+
+  return (
+    <Row name={displayName} href={href} highlighted={isCurrentStrategy}>
+      &hellip;{truncatedAddress}
+    </Row>
+  );
+});
+
+function removeAddressMiddle(addr: string) {
+  return `${addr.slice(0, ADDRESS_SLICE_POINT_A)}…${addr.slice(ADDRESS_SLICE_POINT_B)}`;
+}
+
+ValidatorGroupRow.displayName = 'ValidatorGroupRow';
