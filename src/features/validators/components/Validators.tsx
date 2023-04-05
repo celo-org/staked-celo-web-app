2f4de8d3a5dc22a5f3b1167ac890abc7a94541e3
Aaron DeRuvo <aaron.deruvo@gmail.com>
2023-04-05 15:50:04 -0500

    Fetch Validator Groups (#113)
    * Fetch Validator Groups (no filtering yet)
    
    * add info about exactly what will be filtered
    
    * add graph request for querying block explorer for validators
    
    * fetech validators groups for correct chain
    add celo explorer graphql endpoints
    
    * removing as React celo now does switch the network even after changing chain 10 times.
    
    * move to components directory and fix issue when name is undefined
    
    * clean url means celo main
    
    * remove commented out code
    
    * prepare for merge
    
    * review suggestion: name is falsey unless its at least once char
    
    * chainId in query across all pages so links work nicer
    
    * fix infinite loop now that its on multiple pages
    
    ---------
    
    Co-authored-by: Aaron <aaron.deruvo@clabs.co>

--- src/features/validators/components/Validators.tsx
+++ src/features/validators/components/Validators.tsx
@@ -1,72 +1,14 @@
-import { ValidatorGroupRow } from 'src/features/validators/ValidatorGroupRow';
+import { useAccountContext } from 'src/contexts/account/AccountContext'
+import { ValidatorGroupRow } from 'src/features/validators/components/ValidatorGroupRow'
+import { ValidatorGroup } from 'src/features/validators/data/fetchValidGroups'
 
-const groups = [
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-  {
-    name: 'Figment',
-    address: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
-  },
-];
-export const Validators = () => {
+interface ValidatorsProps {
+  list: ValidatorGroup[];
+}
+
+export const Validators = ({ list }: ValidatorsProps) => {
+  const _ctx = useAccountContext();
+
   return (
     <ul className="flex flex-col justify-center w-full bg-secondary mt-2 p-2 rounded-[16px] gap-2">
       <ValidatorGroupRow
@@ -74,9 +16,9 @@ export const Validators = () => {
         groupAddress="0x0000000000000000000000000000000000000000"
         isCurrentStrategy={true}
       />
-      {groups.map((vg) => (
+      {list.map((vg) => (
         <ValidatorGroupRow key={vg.address} name={vg.name} groupAddress={vg.address} />
       ))}
     </ul>
-  );
-};
+  )
+}
