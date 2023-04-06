479b855aa2ee8d9b8727750a59e3ed95b655a9b5
Aaron <aaron.deruvo@clabs.co>
2023-04-06 14:08:36 -0500

    change language

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -43,7 +43,7 @@ const getText = (mode: Mode) => {
       return 'Stake';
     case Mode.validators:
       '';
-      return 'Vote';
+      return 'Delegate';
     default:
       return 'Submit';
   }
