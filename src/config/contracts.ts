926a5a0638326b7e1cc65ae32d222091161ca1d9
Aaron <aaron.deruvo@clabs.co>
2023-04-20 16:42:14 -0500

    add todo

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -1,4 +1,5 @@
 import {
+  DEFAULT_STRATEGY_MAINNET_ADDRESS,
   DEFAULT_STRATEGY_TESTNET_ADDRESS,
   NEXT_PUBLIC_ACCOUNT_MAINNET_ADDRESS,
   NEXT_PUBLIC_ACCOUNT_TESTNET_ADDRESS,
@@ -16,7 +17,7 @@ interface ContractAddresses {
 }
 
 export const mainnetAddresses: ContractAddresses = {
-  defaultStrategy: '',
+  defaultStrategy: DEFAULT_STRATEGY_MAINNET_ADDRESS,
   manager: NEXT_PUBLIC_MANAGER_MAINNET_ADDRESS as string,
   stakedCelo: NEXT_PUBLIC_STAKED_CELO_MAINNET_ADDRESS as string,
   account: NEXT_PUBLIC_ACCOUNT_MAINNET_ADDRESS as string,
