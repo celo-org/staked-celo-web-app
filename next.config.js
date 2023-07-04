7f285b7f38f23000c96707f4d68a86dad6eb54a4
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2023-07-04 19:38:38 +0200

    fix: remove minimization

--- next.config.js
+++ next.config.js
@@ -4,17 +4,44 @@ const webpack = require('webpack');
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.net = false;
-      config.resolve.fallback.fs = false;
-    }
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
     config.plugins.push(
       new webpack.BannerPlugin({
         banner: 'For third party licenses check /THIRD_PARTY_LICENSES.txt',
       })
     );
+    config.optimization.minimizer = [];
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, content-type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/manifest.json',
+        destination: '/site.webmanifest',
+      },
+    ];
   },
 };
 
