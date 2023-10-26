93351fa8a96b5886177bbd55662d959f87dd0393
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2023-10-26 15:30:37 +0200

    feat: remove past proposals as it's wrong as it stands (#185)
    * feat: remove past proposals as it's wrong as it stands
    
    * fix: eslint
    
    * style: start proposals at the top rather than center of screen

--- src/features/governance/data/getProposals.ts
+++ src/features/governance/data/getProposals.ts
@@ -40,10 +40,13 @@ export const getProposals = async (chainId: number) => {
   );
 
   const current = proposals.filter((x) => runningProposalStages.has(x.stage));
-  const passed = proposals
-    .filter((x) => pastProposalStages.has(x.stage))
-    .sort((a, b) => (parseInt(a.proposalID, 10) < parseInt(b.proposalID, 10) ? 1 : -1))
-    .slice(0, 5);
+  // TODO: this is kinda wrong, proposals that passed are not in the dequeue
+  // so this filter only gets expired and none voted proposals
+  // const passed = proposals
+  //   .filter((x) => pastProposalStages.has(x.stage))
+  //   .sort((a, b) => (parseInt(a.proposalID, 10) < parseInt(b.proposalID, 10) ? 1 : -1))
+  //   .slice(0, 5);
+  const passed = [] as MiniProposal[];
 
   const relevantProposals = [...current, ...passed];
   const metadataCalls = relevantProposals.map((proposal) => ({
@@ -86,7 +89,7 @@ export const getProposals = async (chainId: number) => {
 };
 
 const runningProposalStages = new Set([ProposalStage.Queued, ProposalStage.Referendum]);
-const pastProposalStages = new Set([ProposalStage.Expiration, ProposalStage.Execution]);
+// const pastProposalStages = new Set([ProposalStage.Expiration, ProposalStage.Execution]);
 
 export const getProposalRecord = async (
   chainId: number,
