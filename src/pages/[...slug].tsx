575ad8f8859d53106395d1c81eb08af00f5ee541
Aaron DeRuvo <aaron.deruvo@clabs.co>
2024-05-01 14:02:55 +0200

    when you commit but didnt save

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,5 +1,4 @@
 import type { GetServerSideProps, NextPage } from 'next';
-import { headers } from 'next/headers';
 import Router from 'next/router';
 import { useLayoutEffect, useMemo } from 'react';
 import { Switcher } from 'src/components/switcher/Switcher';
@@ -32,10 +31,6 @@ const MultiModePage: NextPage<Props> = ({
 }) => {
   const { slug } = Router.query as { slug?: string[] };
   const mode = (slug ? slug[0] : Mode.stake) as Mode;
-
-  const headersList = headers();
-
-  console.log('headers', headersList);
 
   useRedirectToConnectedChainIfNeeded(serverSideChainId, mode);
   const isTransitioning = useIsTransitioning();
