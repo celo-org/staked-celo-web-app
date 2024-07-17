3d2c050c6474c52fa515ed3fbc3d872ad9305769
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2024-07-17 22:08:22 +0200

    feat: allow select addresses to vote despite having locked vote balance

---
+++ src/features/validators/components/validator-voters-exceptions.ts
@@ -0,0 +1,17 @@
+// Results from https://dune.com/queries/3923234/6595515
+
+const VALIDATOR_VOTERS_EXCEPTIONS = [
+  '0x6b72b3a6d285abdbfd83055638b445aae3cec786',
+  '0x6f5a6c5aba8af945220111475f907132d02a4b2b',
+  '0xce22f4b16d95127883d6a58ff91dd71ceba50ad7',
+  '0xc8b19839ae371bd541f20b15c3a3cb82bfb6a6c6',
+  '0x69914cc4b87781632fde9e3878650ed77e35e192',
+  '0x27b183925448d1866fc5b3246ca1c97492179ba4',
+  '0x0a22936d3a9e01d22a426426a18c13abef6d4d8e',
+  '0x8c4bd3400800a5a635ce898b14128bad76d19c8c',
+  '0xde7715f2ecfcfa419d30bedd4c366a8734a5ab47',
+  '0xd31459bbe9c4ac947a808f062897aa5b45a07b0c',
+  '0x8066fb4d01b9adad77a783465b4eb8b128073a0d',
+] as `0x${string}`[];
+
+export default VALIDATOR_VOTERS_EXCEPTIONS;
