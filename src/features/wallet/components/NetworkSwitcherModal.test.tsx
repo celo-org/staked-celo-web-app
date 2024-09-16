8879d0f320c1d258ac5038675d10b7298b1ee037
Aaron DeRuvo <aaron.deruvo@clabs.co>
2024-09-16 12:12:50 +0200

    fix test for wagmi v2

--- src/features/wallet/components/NetworkSwitcherModal.test.tsx
+++ src/features/wallet/components/NetworkSwitcherModal.test.tsx
@@ -1,23 +1,22 @@
-import { render } from "@testing-library/react";
-import { NetworkSwitcherModal } from "src/features/wallet/components/NetworkSwitcherModal";
-import { describe, expect, test, vi } from "vitest";
+import { render } from '@testing-library/react';
+import { NetworkSwitcherModal } from 'src/features/wallet/components/NetworkSwitcherModal';
+import { celo, celoAlfajores } from 'viem/chains';
+import { describe, expect, test, vi } from 'vitest';
 
 describe('<NetworkSwitcherModal />', () => {
   test('it shows unambiguous networks names on open modal', () => {
     vi.mock('wagmi', () => ({
       useChainId: () => 42220,
-      useSwitchNetwork: () => { return {} },
+      useSwitchChain: () => {
+        return { chains: [celo, celoAlfajores], status: 'idle', switchChain: () => null };
+      },
     }));
 
-    const result = render(
-      (
-        <NetworkSwitcherModal isOpen={true} close={() => null} />
-      )
-    );
+    const result = render(<NetworkSwitcherModal isOpen={true} close={() => null} />);
 
-    expect(result.getAllByText("Alfajores Testnet")).toHaveLength(1);
-    expect(result.getAllByText("Celo Mainnet")).toHaveLength(1);
+    expect(result.getAllByText('Alfajores Testnet')).toHaveLength(1);
+    expect(result.getAllByText('Celo Mainnet')).toHaveLength(1);
 
-    vi.clearAllMocks();  
+    vi.clearAllMocks();
   });
 });
