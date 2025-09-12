f2e632515e3d4670af4a61a4c6a1c43a57b26342
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-09-12 18:20:11 +0200

    refactor: using wc internals

--- src/utils/walletconnect.ts
+++ src/utils/walletconnect.ts
@@ -1,13 +1,4 @@
-function promisifyIDB<T>(request: IDBRequest<T>): Promise<T> {
-  return new Promise((resolve, reject) => {
-    request.onsuccess = function () {
-      resolve(this.result);
-    };
-    request.onerror = function () {
-      reject(this.error);
-    };
-  });
-}
+import KeyValueStorage from '@walletconnect/keyvaluestorage';
 
 export async function walletConnectCleanup() {
   try {
@@ -18,20 +9,17 @@ export async function walletConnectCleanup() {
       setTimeout(resolve, 1000);
     });
 
-    const request = indexedDB.open('WALLET_CONNECT_V2_INDEXED_DB', 3);
-    const WC_TABLE_NAME = 'keyvaluestorage';
+    const db = new KeyValueStorage({
+      database: 'WALLET_CONNECT_V2_INDEXED_DB',
+      table: 'keyvaluestorage',
+    });
 
-    const db = await promisifyIDB(request);
-    const tx = db.transaction(WC_TABLE_NAME, 'readwrite');
-    const keychainStorage = tx.objectStore(WC_TABLE_NAME);
-
-    const keys = await promisifyIDB(keychainStorage.getAllKeys());
-    const keychainKeys = keys.filter((key) => key.toString().includes('keychain'));
-    for (const key of keychainKeys) {
-      const value: string = await promisifyIDB(keychainStorage.get(key));
-      if (value.includes('client_ed25519_seed')) {
+    const keys = await db.getEntries();
+    const keychainEntries = keys.filter(([key]) => key.includes('keychain'));
+    for (const [key, value] of keychainEntries) {
+      if ('client_ed25519_seed' in value) {
         console.log('deleting', key);
-        await promisifyIDB(keychainStorage.delete(key));
+        await db.removeItem(key);
       }
     }
   } catch (e) {
